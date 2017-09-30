29aaf11c364da9c61421c6890b9dcc92b59970ad
sudheerj <jsudheer50@gmail.com>
2017-09-30 15:53:38 +0800

    The source and destination path corrected

--- generators/app/index.js
+++ generators/app/index.js
@@ -120,7 +120,7 @@ module.exports = JhipsterGenerator.extend({
         const CLIENT_MAIN_SRC_DIR = jhipsterConstants.CLIENT_MAIN_SRC_DIR;
         const CLIENT_TEST_SRC_DIR = jhipsterConstants.CLIENT_TEST_SRC_DIR;
 
-        this.template(CLIENT_MAIN_SRC_DIR + 'content/scss/vendor.scss', CLIENT_MAIN_SRC_DIR + 'content/scss/vendor.scss');
+        this.template(CLIENT_MAIN_SRC_DIR + 'app/content/scss/vendor.scss', CLIENT_MAIN_SRC_DIR + 'app/content/scss/vendor.scss');
 
      /*   this.rewriteFile(
             'src/test/javascript/protractor.conf.js',
