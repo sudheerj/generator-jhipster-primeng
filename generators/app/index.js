52da77659a7901697d95385cdf9cac2b5cb8d114
sudheerj <jsudheer50@gmail.com>
2017-09-30 12:47:11 +0800

    Access Jhipster config

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,6 +5,7 @@ const packagejs = require('../../package.json');
 const semver = require('semver');
 const shelljs = require('shelljs');
 const BaseGenerator = require('generator-jhipster/generators/generator-base');
+const jhipsterConstants = require('generator-jhipster/generators/generator-constants');
 
 const JhipsterGenerator = generator.extend({});
 util.inherits(JhipsterGenerator, BaseGenerator);
