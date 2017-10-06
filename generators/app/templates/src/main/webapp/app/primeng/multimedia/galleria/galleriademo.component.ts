d5eab7d3dcb51faac3e23e28b8fd5cb681605e9e
sudheerj <jsudheer50@gmail.com>
2017-10-06 09:45:13 +0800

    Resources structure modified

--- generators/app/templates/src/main/webapp/app/primeng/multimedia/galleria/galleriademo.component.ts
+++ generators/app/templates/src/main/webapp/app/primeng/multimedia/galleria/galleriademo.component.ts
@@ -16,37 +16,37 @@ export class GalleriaDemoComponent implements OnInit {
     ngOnInit() {
         this.images = [];
         this.images.push({
-            source: '/assets/data/images/cars/Yeni.png',
+            source: '/content/primeng/assets/data/images/cars/Yeni.png',
             alt: 'This is a first car',
             title: 'Yeni Vollkswagen CC'
         });
         this.images.push({
-            source: '/assets/data/images/cars/Golf.png',
+            source: '/content/primeng/assets/data/images/cars/Golf.png',
             alt: 'This is a second car',
             title: 'Golf'
         });
         this.images.push({
-            source: '/assets/data/images/cars/Jetta.png',
+            source: '/content/primeng/assets/data/images/cars/Jetta.png',
             alt: 'This is a third car',
             title: 'Jetta'
         });
         this.images.push({
-            source: '/assets/data/images/cars/Passat.png',
+            source: '/content/primeng/assets/data/images/cars/Passat.png',
             alt: 'This is a fourth car',
             title: 'Passat'
         });
         this.images.push({
-            source: '/assets/data/images/cars/Polo.png',
+            source: '/content/primeng/assets/data/images/cars/Polo.png',
             alt: 'This is a fifth car',
             title: 'Polo'
         });
         this.images.push({
-            source: '/assets/data/images/cars/Scirocco.png',
+            source: '/content/primeng/assets/data/images/cars/Scirocco.png',
             alt: 'This is a sixth car',
             title: 'Scirocco'
         });
         this.images.push({
-            source: '/assets/data/images/cars/Touareg.png',
+            source: '/content/primeng/assets/data/images/cars/Touareg.png',
             alt: 'This is a seventh car',
             title: 'Touareg'
         });
