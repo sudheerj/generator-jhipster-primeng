8e309c1e33f214106efefed3c8ad900192b6f8ce
sudheerj <jsudheer50@gmail.com>
2017-10-17 02:09:49 +0800

    End tag added for link HTML tag

--- generators/app/index.js
+++ generators/app/index.js
@@ -976,7 +976,7 @@ module.exports = JhipsterGenerator.extend({
 
         const primengResources = `\n@import "~primeng/resources/primeng.min.css";\n @import "~primeng/resources/themes/${themeName}/theme.css";\n @import "~quill/dist/quill.core.css";\n @import "~quill/dist/quill.snow.css";`;
         const primengRootResources = `<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-                                      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.css">
+                                      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.css"/>
                                       <script src="https://www.google.com/recaptcha/api.js?render=explicit&onload=initRecaptcha" async defer></script>
                                       <script src="https://cdn.quilljs.com/1.0.0/quill.js"></script>  
                                       <script type="text/javascript" src="https://maps.google.com/maps/api/js?key=AIzaSyA6Ar0UymhiklJBzEPLKKn2QHwbjdz3XV0" ></script>
