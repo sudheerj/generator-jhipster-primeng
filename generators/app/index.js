9a024b890c1277af5b4547d5fa5576988c518373
sudheerj <jsudheer50@gmail.com>
2017-10-28 00:27:19 +0800

    PrimeNG version updated

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,7 +13,7 @@ const JhipsterGenerator = generator.extend({});
 util.inherits(JhipsterGenerator, BaseGenerator);
 
 const ANGULAR_VERSION = '4.2.6';
-const PRIMENG_VERSION = '4.2.2';
+const PRIMENG_VERSION = '4.3.0-RC1';
 const PRIMENG_EXT_WIZARD_VERSION = '2.1.0';
 const CHARTJS_VERSION = '2.6.0';
 const MOMENT_VERSION = '2.18.1';
