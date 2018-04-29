fa5a6fbe9d3503d7bba7687114adfc25fb5e49da
sudheerj <jsudheer50@gmail.com>
2018-04-29 22:01:09 +0530

    Upgrade to PrimeNG 5.2.5

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,7 +13,7 @@ const JhipsterGenerator = generator.extend({});
 util.inherits(JhipsterGenerator, BaseGenerator);
 
 const ANGULAR_VERSION = '5.2.10';
-const PRIMENG_VERSION = '5.2.4';
+const PRIMENG_VERSION = '5.2.5';
 const PRIMENG_EXTENSIONS_VERSION = '0.0.39';
 const CHARTJS_VERSION = '2.7.1';
 const MOMENT_VERSION = '2.20.1';
