9c3fa87cfa80a90a36705e2928bd9efcc49e0593
sudheerj <jsudheer50@gmail.com>
2018-09-23 23:47:51 +0800

    Update external dependencies

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,7 +13,7 @@ const JhipsterGenerator = generator.extend({});
 util.inherits(JhipsterGenerator, BaseGenerator);
 
 const ANGULAR_VERSION = '6.1.0';
-const PRIMENG_VERSION = '6.1.3';
+const PRIMENG_VERSION = '6.1.4';
 const PRIMEICONS_VERSION = '1.0.0-beta.10';
 const PRIMEFLEX_VERSION = '1.0.0-rc.1';
 const PRIMENG_EXTENSIONS_VERSION = '0.0.45';
@@ -1051,14 +1051,13 @@ module.exports = JhipsterGenerator.extend({
         };
 
         const primengResources = `\n@import "~primeicons/primeicons.css";\n@import "~primeicons/primeicons.css";\n@import "~primeng/resources/primeng.min.css";\n@import "~primeng-extensions/resources/primeng-extensions.min.css";\n @import "~primeng/resources/themes/${themeName}/theme.css";\n @import "~quill/dist/quill.core.css";\n @import "~quill/dist/quill.snow.css";`;
-        const primengRootResources = `<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-                                      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-                                      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.css"/>
+        const primengRootResources = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                                      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css"/>
                                       <script src="https://www.google.com/recaptcha/api.js?render=explicit&onload=initRecaptcha" async defer></script>
-                                      <script src="https://cdn.quilljs.com/1.0.0/quill.js"></script>  
+                                      <script src="https://cdn.quilljs.com/1.1.8/quill.js"></script>  
                                       <script type="text/javascript" src="https://maps.google.com/maps/api/js?key=AIzaSyA6Ar0UymhiklJBzEPLKKn2QHwbjdz3XV0" ></script>
-                                      <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min.js"></script>
-                                      <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.js"></script>\n`;
+                                      <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.19.0/moment.min.js"></script>
+                                      <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>\n`;
 
         if (this.copyExternalAssetsInWebpack) {
             this.copyExternalAssetsInWebpack('primeng', 'primeng');
