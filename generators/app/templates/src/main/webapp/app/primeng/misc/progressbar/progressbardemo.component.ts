7f7aa5eae9808380a3178c1e0f5813b5775c0297
sudheerj <jsudheer50@gmail.com>
2018-11-24 09:32:39 +0800

    Apply RXJS6 changes

--- generators/app/templates/src/main/webapp/app/primeng/misc/progressbar/progressbardemo.component.ts
+++ generators/app/templates/src/main/webapp/app/primeng/misc/progressbar/progressbardemo.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { JhiLanguageService } from 'ng-jhipster';
 import {MenuItem, Message} from 'primeng/components/common/api';
-import {Observable} from 'rxjs/Observable';
 import {Subscription} from 'rxjs';
+import { interval } from 'rxjs';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/take';
 
@@ -19,8 +19,8 @@ export class ProgressBarDemoComponent implements OnInit, OnDestroy {
     interval$: Subscription;
 
     ngOnInit() {
-        const interval = Observable.interval(800).take(100);
-        this.interval$ = interval.subscribe(
+        const intervalObject = interval(800).take(100);
+        this.interval$ = intervalObject.subscribe(
             x => this.value = x + 1,
             () => {/** no error handling */ },
             () => this.msgs = [{severity: 'info', summary: 'Success', detail: 'Process completed'}]
